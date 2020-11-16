0fe309ab8708d6cc6bf96a63be02d97064eb5b67
BichHoang <bichht95@gmail.com>
2020-11-16 15:13:20 +0700

    list view basic

--- app/src/main/java/com/example/learnandroidfrom0/MainActivity.java
+++ app/src/main/java/com/example/learnandroidfrom0/MainActivity.java
@@ -3,12 +3,83 @@ package com.example.learnandroidfrom0;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayList<String> languages;
+    Button btnAdd, btnEdit, btnDelete;
+    EditText textLanguage;
+    int posEdit = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        btnAdd = findViewById(R.id.btnAdd);
+        textLanguage = findViewById(R.id.txtInput_language);
+        btnEdit = findViewById(R.id.btnEdit);
+        btnDelete = findViewById(R.id.btnDelete);
+
+        languages = new ArrayList<>();
+        languages.add("English");
+        languages.add("Vietnamese");
+        languages.add("Japanese");
+        languages.add("China");
+        languages.add("Laos");
+        languages.add("Thai");
+
+        final ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, languages);
+
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                textLanguage.setText(languages.get(position));
+                posEdit = position;
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Long click: " + position, Toast.LENGTH_SHORT).show();
+                return  false;
+            }
+        });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                languages.add(textLanguage.getText().toString());
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                languages.set(posEdit, textLanguage.getText().toString());
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                languages.remove(posEdit);
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
